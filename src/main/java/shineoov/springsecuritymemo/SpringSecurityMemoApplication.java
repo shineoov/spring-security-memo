66f5b752ada525016392dc17acccdab0bb71a980
shineoov <shineoov88@gamil.com>
2022-01-08 18:29:25 +0900

    Project Initialize

---
+++ src/main/java/shineoov/springsecuritymemo/SpringSecurityMemoApplication.java
@@ -0,0 +1,13 @@
+package shineoov.springsecuritymemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shineoov/springsecuritymemo/SpringSecurityMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package shineoov.springsecuritymemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
