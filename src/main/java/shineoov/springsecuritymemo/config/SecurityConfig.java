6ce2242b3ef480bb2fc90685d243cfe89143da4e
shineoov <shineoov88@gmail.com>
2022-01-10 01:47:03 +0900

    FormLogin 커스텀 기능 추가

--- src/main/java/shineoov/springsecuritymemo/config/SecurityConfig.java
+++ src/main/java/shineoov/springsecuritymemo/config/SecurityConfig.java
@@ -1,17 +1,30 @@
 package shineoov.springsecuritymemo.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Slf4j
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+
+        // form login 설정
         http.formLogin(form -> form
-                .loginPage("/login")
+                //.loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .successHandler((request, response, authentication) -> {
+                    log.info("[LoginSuccess] name={}", authentication.getName());
+                    log.info("[LoginSuccess] principal={}", authentication.getPrincipal());
+                })
+                .failureHandler((request, response, exception) -> {
+                    log.info("[LoginFail] exceptionMessage={}", exception.getMessage());
+                })
                 .permitAll()
         );
         http.httpBasic();
