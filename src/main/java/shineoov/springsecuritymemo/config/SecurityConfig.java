fe661e0ec0f504b29dace6e4dd236aad202198b6
shineoov <shineoov88@gmail.com>
2022-01-10 14:49:49 +0900

    Logout 설정

--- src/main/java/shineoov/springsecuritymemo/config/SecurityConfig.java
+++ src/main/java/shineoov/springsecuritymemo/config/SecurityConfig.java
@@ -11,9 +11,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeRequests((requests) -> requests
+                .mvcMatchers("/").permitAll()
+                .anyRequest().authenticated()
+        );
 
-        // form login 설정
+        http.httpBasic();
+
+        configureFormLogin(http);
+
+        configureLogout(http);
+
+        configureSession(http);
+    }
+
+    private void configureLogout(HttpSecurity http) throws Exception {
+        //https://docs.spring.io/spring-security/reference/servlet/authentication/logout.html
+        http.logout(logout -> logout
+                .deleteCookies("cookie1", "cookie2")
+                .logoutSuccessHandler((request, response, authentication) -> {
+                    log.info("[LOGOUT] authentication={}", authentication);
+                    response.sendRedirect("/?referer=logout");
+                })
+        );
+    }
+
+    private void configureSession(HttpSecurity http) throws Exception {
+        http.sessionManagement(session -> session
+                .maximumSessions(1) // 최대 Session 수
+                .maxSessionsPreventsLogin(false) // true -> 추가 로그인 X
+        );
+    }
+
+    private void configureFormLogin(HttpSecurity http) throws Exception {
         http.formLogin(form -> form
                 //.loginPage("/login")
                 .usernameParameter("username")
@@ -26,13 +56,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     log.info("[LoginFail] exceptionMessage={}", exception.getMessage());
                 })
                 .permitAll()
-        );
-        http.httpBasic();
-
-        //SESSION 설정
-        http.sessionManagement(session -> session
-                .maximumSessions(1) // 최대 Session 수
-                .maxSessionsPreventsLogin(false) // true -> 추가 로그인 X
         );
     }
 }
